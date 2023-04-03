4acb2d59ccf0c77190a2c3d1b7abaf49e1c1b26a
Akash Kedia <kedia23akash@gmail.com>
2023-04-02 22:19:48 -0700

    nat gateway

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import { AwsCredentialsProvider } from '@tinystacks/ops-aws-core-widgets';
-import { AwsCloudwatchLogsUtilization } from './service-utilizations/aws-cloudwatch-logs-utilization.js';
+// import { AwsCredentialsProvider } from '@tinystacks/ops-aws-core-widgets';
+// import { AwsCloudwatchLogsUtilization } from './service-utilizations/aws-cloudwatch-logs-utilization.js';
+// import { AwsNatGatewayUtilization } from './service-utilizations/aws-nat-gateway-utilization.js';
 
-const cwLogs = new AwsCloudwatchLogsUtilization();
+// const cwLogs = new AwsCloudwatchLogsUtilization();
+// const natGateway = new AwsNatGatewayUtilization();
 
-const credentialsProvider = AwsCredentialsProvider.fromJson({
-  id: 'thing',
-  type: 'AwsCredentialsProvider',
-  credentials: {
-    profileName: 'akash-dev'
-  }
-});
+// const credentialsProvider = AwsCredentialsProvider.fromJson({
+//   id: 'thing',
+//   type: 'AwsCredentialsProvider',
+//   credentials: {
+//     profileName: 'ts'
+//   }
+// });
 
-cwLogs.getAssessment(credentialsProvider, 'us-east-1');
+// natGateway.getUtilization(credentialsProvider, 'us-east-1');
--- src/service-utilizations/aws-nat-gateway-utilization.ts
+++ src/service-utilizations/aws-nat-gateway-utilization.ts
@@ -1,41 +1,140 @@
-// import { CloudWatch } from "@aws-sdk/client-cloudwatch";
-// import { AwsCredentialsProvider } from "@tinystacks/ops-aws-core-widgets";
-// import { AwsServiceUtilization } from "./aws-service-utilization.js";
+import { CloudWatch } from '@aws-sdk/client-cloudwatch';
+import { EC2 } from '@aws-sdk/client-ec2';
+import { AwsCredentialsProvider } from '@tinystacks/ops-aws-core-widgets';
+import _ from 'lodash';
+import { AlertType } from '../types/types.js';
+import { AwsServiceUtilization } from './aws-service-utilization.js';
 
-// type AwsNatGatewayUtilizationScenarioTypes = {
-//   activeConnectionCount: number,
-//   totalThroughput: number
-// }
+type AwsNatGatewayUtilizationScenarioTypes = {
+  activeConnectionCount: number,
+  totalThroughput: number
+}
 
-// export class AwsNatGatewayUtilization extends AwsServiceUtilization<AwsNatGatewayUtilizationScenarioTypes> {
+export class AwsNatGatewayUtilization extends AwsServiceUtilization<AwsNatGatewayUtilizationScenarioTypes> {
 
-//   async getAssessment(awsCredentialsProvider: AwsCredentialsProvider, region: string, overrides?: any) {
-//     const cwClient = new CloudWatch({
-//       credentials: await awsCredentialsProvider.getCredentials(),
-//       region
-//     });
+  async getUtilization (awsCredentialsProvider: AwsCredentialsProvider, region: string, _overrides?: any) {
+    const credentials = await awsCredentialsProvider.getCredentials();
+    const ec2Client = new EC2({
+      credentials,
+      region
+    });
+    const cwClient = new CloudWatch({
+      credentials,
+      region
+    });
 
-//     const res = await cwClient.getMetricData({
-//       MetricDataQueries: [
-//         {
-//           Id: 'activeConnectionCount',
-//           Metrics: 
-//         },
-//         {
-//           Id: 'bytesInFromDestination'
-//         },
-//         {
-//           Id: 'bytesInFromSource'
-//         },
-//         {
-//           Id: 'bytesOutToDestination'
-//         },
-//         {
-//           Id: 'bytesOutToSource'
-//         }
-//       ],
-//       StartTime: new Date(),
-//       EndTime: new Date()
-//     });
-//   }
-// }
+    const res = await ec2Client.describeNatGateways({});
+    const natGatewayIds = res.NatGateways.map(natGateway => natGateway.NatGatewayId);
+    const fiveMinutesAgo = new Date(Date.now() - (5 * 60 * 1000));
+    for (const natGatewayId of natGatewayIds) {
+      const metricDataRes = await cwClient.getMetricData({
+        MetricDataQueries: [
+          {
+            Id: 'activeConnectionCount',
+            MetricStat: {
+              Metric: {
+                Namespace: 'AWS/NATGateway',
+                MetricName: 'ActiveConnectionCount',
+                Dimensions: [{
+                  Name: 'NatGatewayId',
+                  Value: natGatewayId
+                }]
+              },
+              Period: 5 * 60, // 5 minutes
+              Stat: 'Maximum'
+            }
+          },
+          {
+            Id: 'bytesInFromDestination',
+            MetricStat: {
+              Metric: {
+                Namespace: 'AWS/NATGateway',
+                MetricName: 'BytesInFromDestination',
+                Dimensions: [{
+                  Name: 'NatGatewayId',
+                  Value: natGatewayId
+                }]
+              },
+              Period: 5 * 60, // 5 minutes
+              Stat: 'Sum'
+            }
+          },
+          {
+            Id: 'bytesInFromSource',
+            MetricStat: {
+              Metric: {
+                Namespace: 'AWS/NATGateway',
+                MetricName: 'BytesInFromSource',
+                Dimensions: [{
+                  Name: 'NatGatewayId',
+                  Value: natGatewayId
+                }]
+              },
+              Period: 5 * 60, // 5 minutes
+              Stat: 'Sum'
+            }
+          },
+          {
+            Id: 'bytesOutToDestination',
+            MetricStat: {
+              Metric: {
+                Namespace: 'AWS/NATGateway',
+                MetricName: 'BytesOutToDestination',
+                Dimensions: [{
+                  Name: 'NatGatewayId',
+                  Value: natGatewayId
+                }]
+              },
+              Period: 5 * 60, // 5 minutes
+              Stat: 'Sum'
+            }
+          },
+          {
+            Id: 'bytesOutToSource',
+            MetricStat: {
+              Metric: {
+                Namespace: 'AWS/NATGateway',
+                MetricName: 'BytesOutToSource',
+                Dimensions: [{
+                  Name: 'NatGatewayId',
+                  Value: natGatewayId
+                }]
+              },
+              Period: 5 * 60, // 5 minutes
+              Stat: 'Sum'
+            }
+          }
+        ],
+        StartTime: fiveMinutesAgo,
+        EndTime: new Date()
+      });
+
+      const results = metricDataRes.MetricDataResults;
+      const activeConnectionCount = _.get(results, '[0].Values[0]');
+      if (activeConnectionCount === 0) {
+        this.addScenario(natGatewayId, 'activeConnectionCount', {
+          value: activeConnectionCount,
+          alertType: AlertType.Alarm,
+          reason: 'this NAT Gateway currently has 0 active connections',
+          recommendation: 'review this NAT Gateway and the Route Tables associated with its VPC. If another NAT Gateway exists in the VPC, repoint routes to that gateway and delete this gateway. If this is the only NAT Gateway in your VPC and resources depend on network traffic, retain this gateway.',
+          actions: []
+        });
+      }
+      const totalThroughput = 
+        _.get(results, '[1].Values[0]', 0) + 
+        _.get(results, '[2].Values[0]', 0) + 
+        _.get(results, '[3].Values[0]', 0) +
+        _.get(results, '[4].Values[0]', 0);
+      if (totalThroughput === 0) {
+        this.addScenario(natGatewayId, 'totalThroughput', {
+          value: activeConnectionCount,
+          alertType: AlertType.Alarm,
+          reason: 'this NAT Gateway currently has 0 total throughput',
+          recommendation: 'review this NAT Gateway and the Route Tables associated with its VPC. If another NAT Gateway exists in the VPC, repoint routes to that gateway and delete this gateway. If this is the only NAT Gateway in your VPC and resources depend on network traffic, retain this gateway.',
+          actions: []
+        });
+      }
+    }
+    console.log(this.utilization);
+  }
+}
