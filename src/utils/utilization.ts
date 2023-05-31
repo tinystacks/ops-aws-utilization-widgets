91dd44282c9596b59fef5b52b37007dfd8268778
nefertitirogers <nefertitirogers@gmail.com>
2023-05-31 14:49:19 -0700

    update

--- src/utils/utilization.ts
+++ src/utils/utilization.ts
@@ -63,6 +63,18 @@ export function getTotalNumberOfResources ( utilization: { [service: string]: Ut
   return total;
 }
 
+export function getTotalMonthlySavings (utilization: { [service: string]: Utilization<string> }): number { 
+  let totalSavings = 0; 
+  Object.keys(utilization).forEach((service) => {
+    if (!utilization[service] || isEmpty(utilization[service])) return;
+    Object.keys(utilization[service]).forEach((resource) => { 
+      totalSavings += utilization[service][resource].data?.maxMonthlySavings;
+    });
+  });
+
+  return totalSavings;
+}
+
 export function sentenceCase (name: string): string{ 
   const result = name.replace(/([A-Z])/g, ' $1');
   return result[0].toUpperCase() + result.substring(1).toLowerCase();
