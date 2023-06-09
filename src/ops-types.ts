c5f2540b9fad1129a28432864850790dd7131fed
Caleb Courier <caleb.courier@gmail.com>
2023-06-09 15:12:34 -0500

    add yaml config examples in tsdoc

--- src/ops-types.ts
+++ src/ops-types.ts
@@ -1,5 +1,16 @@
 import { Provider, Widget } from '@tinystacks/ops-model';
 
+/**
+ * @example
+ * ```yaml
+ * AwsUtilizationRecommendations:
+    type: AwsUtilizationRecommendations
+    displayName: TinyStacks Recommendations
+    providers: 
+      - $ref: '#/Console/providers/AwsProvider'
+      - $ref: '#/Console/providers/UtilizationProvider'
+ * ```
+ */
 export interface AwsUtilizationRecommendations extends Widget {
   regions?: string[]
 }
@@ -8,6 +19,13 @@ export interface AwsUtilization extends Widget {
   region?: string
 }
 
+/**
+ * @example
+ * ```yaml
+ * UtilizationProvider:
+    type: AwsUtilizationProvider
+ * ```
+ */
 export interface AwsUtilizationProvider extends Provider {
   services?: AwsResourceType[];
   regions?: string[];
